d703911fb8cd815380d15a8395d858a8802aa223
Ayushi Jain <55890440+ayushi19031@users.noreply.github.com>
2020-04-21 21:44:44 +0530

    My first commit, automation of login in git

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
+const express = require("express");
+const app = express();
+const https = require("https");
+const bodyParser = require("body-parser");
 const puppeteer = require('puppeteer');
-
+var url = require('url');
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/", function(req, res){
+  var username  = req.body.emailid;
+  var password  = req.body.Password;
+  res.send("The information is " + username + " " + password);
+});
+app.listen(3000, function(){
+  console.log("Server is up and running at port 3000");
+//  res.send(username + " " + password);
+});
 (async () => {
   const browser = await puppeteer.launch({
    headless: false
  });
   const page = await browser.newPage();
-  await page.goto('https://github.com/join');
+  await page.goto('https://github.com/login');
+  //console.log("Logged in successfully");
+  await page.type('#login_field', "ayushij2704@gmail.com");
+ await page.type('#password', "ayushij@27");
+ console.log("Logged in successfully");
+ await page.click('[name="commit"]')
 await page.setDefaultNavigationTimeout(0);
-let texts = await page.evaluate(() => {
-  let data = []
-   let elements = document.getElementsByClassName('form-control input-sm header-search-input jump-to-field js-jump-to-field js-site-search-focus');
-   for (var element of elements)
-         data.push(element.name);
-     return data;
-   }
-);
-console.log(texts);
+
+console.log("Yeah!! ");
+await page.goto("https://github.com/settings/tokens");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+const list = await page.evaluate(() =>
+{let x = document.getElementsByClassName("ml-2 details-reset details-overlay details-overlay-dark");
+return x
+});
+console.log(list);
+console.log(page.frames());
 await page.screenshot({ path: "landing-page.png",  fullpage: true});
-//  browser.close();
 })();
